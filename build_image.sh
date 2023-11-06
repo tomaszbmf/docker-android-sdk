fb345ac258bdfcecb50e00fb99a3ba88af7406dd
Tomasz Bugała <tomaszb@mobilefuse.com>
2023-11-06 15:43:58 +0100

    alpine image with minimal Android SDK 33 toolchain it doesn't contails Android NDK nor emulator

---
+++ build_image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ANDROID_API=33
+DOCKER_IMAGE_TAG="androidtools/android-sdk-$ANDROID_API"
+
+echo "Build Docker \"$DOCKER_IMAGE_TAG\" image with minimal Android SDK API $ANDROID_API"
+docker build --tag $DOCKER_IMAGE_TAG . -f alpine/standalone/Dockerfile
--- tools/android-sdk-update.sh
+++ tools/android-sdk-update.sh
@@ -4,21 +4,6 @@ mkdir -p /opt/android-sdk-linux/bin/
 cp /opt/tools/android-env.sh /opt/android-sdk-linux/bin/
 source /opt/android-sdk-linux/bin/android-env.sh
 
-built_in_sdk=1
-
-if [ $# -ge 0 ] && [ "$1" == "lazy-dl" ]
-then
-    echo "Using Lazy Download Flavour"
-    built_in_sdk=0
-elif [ $# -ge 0 ] && [ "$1" == "built-in" ]
-then
-    echo "Using Built-In SDK Flavour"
-    built_in_sdk=1
-else
-    echo "Please use either built-in or lazy-dl as parameter"
-    exit 1
-fi
-
 cd ${ANDROID_HOME}
 echo "Set ANDROID_HOME to ${ANDROID_HOME}"
 
@@ -48,18 +33,10 @@ cp -v /opt/tools/*.sh ${ANDROID_HOME}/bin
 echo "Print sdkmanager version"
 sdkmanager --version
 
-
 echo "Installing packages"
-if [ $built_in_sdk -eq 1 ]
-then
-    while read p; do 
-      android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} ${p}"
-    done < /opt/tools/package-list-minimal.txt
-else
-    while read p; do
-      android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} ${p}"
-    done < /opt/tools/package-list.txt
-fi
+while read p; do 
+    android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} ${p}"
+done < /opt/tools/package-list-minimal.txt
 
 echo "Updating SDK"
 update_sdk
@@ -67,10 +44,3 @@ update_sdk
 echo "Accepting Licenses"
 android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} --licenses"
 
-# https://stackoverflow.com/questions/35128229/error-no-toolchains-found-in-the-ndk-toolchains-folder-for-abi-with-prefix-llv
-if [ -d /opt/android-sdk-linux/ndk-bundle/toolchains ]
-then
-    ( cd /opt/android-sdk-linux/ndk-bundle/toolchains \
-    && ln -sf aarch64-linux-android-4.9 mips64el-linux-android \
-    && ln -sf arm-linux-androideabi-4.9 mipsel-linux-android )
-fi
