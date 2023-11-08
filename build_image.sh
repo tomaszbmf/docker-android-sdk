e1dbc2725a82987441f702b07d89fb77bda433c2
Tomasz Bugała <tomaszb@mobilefuse.com>
2023-11-08 14:10:33 +0100

    use ubuntu as a base

--- build_image.sh
+++ build_image.sh
@@ -3,4 +3,4 @@ ANDROID_API=33
 DOCKER_IMAGE_TAG="androidtools/android-sdk-$ANDROID_API"
 
 echo "Build Docker \"$DOCKER_IMAGE_TAG\" image with minimal Android SDK API $ANDROID_API"
-docker build --tag $DOCKER_IMAGE_TAG . -f alpine/standalone/Dockerfile
+docker build --tag $DOCKER_IMAGE_TAG . -f ubuntu/standalone/Dockerfile
